95a1e9278bf23f1a049a67f548a9254cf400d10d
Razvan Timis <timisrazvanvasile@gmail.com>
2020-08-05 20:00:12 +0300

    Add object array

---
+++ week3/JSBasic/ex2/app.js
@@ -0,0 +1,22 @@
+// object and array
+var name = ['razvan', 'timis'];
+var age = 25;
+var isMen = true;
+var list = [name, age, isMen, 10, 9, 8, 7]
+
+
+// obiect
+var personName = {
+  firstName: 'Cosmin',
+  lastName: 'S'
+}
+console.log(personName)
+var person = {
+  name: personName,
+  age: 28,
+  isMen: true,
+}
+console.log(person)
+
+
+
