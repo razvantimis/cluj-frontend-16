200799431d3076b5adb9f050d804609b8f5978d4
Razvan Timis <timisrazvanvasile@gmail.com>
2020-08-05 19:40:01 +0300

    Add js basic

---
+++ week3/JSBasic/ex1/app.js
@@ -0,0 +1,28 @@
+// number, string, boolean
+var name = 'razvan';
+var age = 25;
+var isMen = true;
+// operatori number
+// sum
+console.log(10 + 2)
+console.log(10 * 2)
+console.log(10 / 2)
+console.log(10 % 2) // modul
+
+console.log(10 == 10)
+console.log(10 != 10)
+console.log(11 > 10)
+console.log(11 < 10)
+console.log(10 <= 10)
+console.log(10 >= 10)
+console.log(age > 11 && age < 15)
+console.log(!(age > 11 || age < 15))
+// operatori string
+console.log('test1' + 'buzz')
+console.log('test1' / 'buzz')
+
+// https://en.wikipedia.org/wiki/Camel_case
+
+
+var result = 11 + 24 + 56 + 100;
+console.log(result)
