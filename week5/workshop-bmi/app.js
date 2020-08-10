0f6c8be91b54f6c2d086d022f2f6efe84be21e0f
Razvan Timis <timisrazvanvasile@gmail.com>
2020-08-10 19:35:55 +0300

    Add v2 bmi

--- week5/workshop-bmi/app.js
+++ week5/workshop-bmi/app.js
@@ -1,3 +1,4 @@
+// version 1
 var height = 1.71;
 var weight = 85;
 
@@ -13,4 +14,29 @@ if (bmi < 18.5) {
   console.log("your weight is overweight");
 } else {
   console.log("your weight is obese");
+}
+
+// version 2
+var Paul = {
+  height: 1.68,
+  weight: 57,
+}
+var Ana = {
+  height: 1.63,
+  weight: 80,
+}
+var list = [Paul, Ana]
+for (var i = 0; i < list.length; i++) {
+  var person = list[i];
+
+  var bmi = person.weight / (person.height * person.height);
+  if (bmi < 18.5) {
+    console.log("your weight is underweight");
+  } else if (bmi < 25) {
+    console.log("your weight is normal");
+  } else if (bmi < 30) {
+    console.log("your weight is overweight");
+  } else {
+    console.log("your weight is obese");
+  }
 }
