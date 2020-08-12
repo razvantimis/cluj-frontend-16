ade528041ccc99b11c3ab7d2e2c208554921739b
Razvan Timis <timisrazvanvasile@gmail.com>
2020-08-12 19:45:07 +0300

    Add

--- week5/workshop-bmi/app.js
+++ week5/workshop-bmi/app.js
@@ -6,45 +6,46 @@ var bmi = weight / (height * height);
 
 console.log(bmi);
 
+
 if (bmi < 18.5) {
-  console.log("your weight is underweight");
+    console.log("your weight is underweight");
 } else if (bmi < 25) {
-  console.log("your weight is normal");
+    console.log("your weight is normal");
 } else if (bmi < 30) {
-  console.log("your weight is overweight");
+    console.log("your weight is overweight");
 } else {
-  console.log("your weight is obese");
+    console.log("your weight is obese");
 }
 
 // version 2
 var Paul = {
-  height: 1.68,
-  weight: 57,
-  name: "Paul"
+    height: 1.68,
+    weight: 57,
+    name: "Paul"
 }
 var Ana = {
-  height: 1.63,
-  weight: 80,
-  name: "Ana"
+    height: 1.63,
+    weight: 80,
+    name: "Ana"
 }
 var list = [Paul, Ana]
 list.push({
-  height: 1.69,
-  weight: 70,
-  name: "Maria"
+    height: 1.69,
+    weight: 70,
+    name: "Maria"
 });
 
 for (var i = 0; i < list.length; i++) {
-  var person = list[i];
+    var person = list[i];
 
-  var bmi = person.weight / (person.height * person.height);
-  if (bmi < 18.5) {
-    console.log(person.name + " your weight is underweight");
-  } else if (bmi < 25) {
-    console.log(person.name + " your weight is normal");
-  } else if (bmi < 30) {
-    console.log(person.name + " your weight is overweight");
-  } else {
-    console.log(person.name + " your weight is obese");
-  }
+    var bmi = person.weight / (person.height * person.height);
+    if (bmi < 18.5) {
+        console.log(person.name + " your weight is underweight");
+    } else if (bmi < 25) {
+        console.log(person.name + " your weight is normal");
+    } else if (bmi < 30) {
+        console.log(person.name + " your weight is overweight");
+    } else {
+        console.log(person.name + " your weight is obese");
+    }
 }
---
+++ week5/workshop-bmi/appv3.js
@@ -0,0 +1,51 @@
+// version 1
+var height = 1.71;
+var weight = 85;
+
+var bmi = calcBmi(weight, height);
+showMsgBmi(bmi);
+
+function calcBmi(weight, height) {
+    return weight / (height * height);
+}
+
+function showMsgBmi(bmi, x) {
+    if (bmi < 18.5) {
+        console.log(x +
+            " your weight is underweight");
+    } else if (bmi < 25) {
+        console.log(x + " your weight is normal");
+    } else if (bmi < 30) {
+        console.log(x + " your weight is overweight");
+    } else {
+        console.log(x + " your weight is obese");
+    }
+}
+
+
+// version 2
+var x = {
+    height: 1.68,
+    weight: 57,
+    name: "Paul"
+}
+var y = {
+    height: 1.63,
+    weight: 80,
+    name: "Ana"
+}
+var list = [x, y]
+list.push({
+    height: 1.69,
+    weight: 70,
+    name: "Maria"
+});
+
+for (var i = 0; i < list.length; i++) {
+    var person = list[i];
+    console.log('person', person)
+
+    var bmi = calcBmi(person.weight, person.height);
+
+    showMsgBmi(bmi, person.name);
+}
