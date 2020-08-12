3138049c6b088a4f85a50c62524aaffd7744bf32
Razvan Timis <timisrazvanvasile@gmail.com>
2020-08-12 21:21:25 +0300

    Add

--- week5/dom-maniplution/contact-form/app.js
+++ week5/dom-maniplution/contact-form/app.js
@@ -1,7 +1,8 @@
 const nameInput = document.querySelector('#name-input');
 
 nameInput.addEventListener('input', function(event){
-  // console.log(event.target.value)
+  // console.log(event.target.value) 
+  // nameInput.value , event.target === nameInput
 })
 
 const button = document.querySelector('#send')
@@ -11,8 +12,11 @@ function sendButton(){
   console.log('sa da click')
   console.log('nameInput', nameInput.value)
 
+  // validare
   if(nameInput.value){
     console.log('este valida')
+  } else {
+    nameInput.classList.add('invalid')
   }
 }
 
