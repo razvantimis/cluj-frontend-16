2def802e1c93c58973bdecd87e5da1d7ecc908aa
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-09 19:00:15 +0300

    Add

--- week9/promise/ex.js
+++ week9/promise/ex.js
@@ -1,3 +1,4 @@
+// v1
 for(var a=0; a<10;a++){
   // a=0, a=1,...a=9,
   setTimeout(start, 0)
@@ -5,5 +6,30 @@ for(var a=0; a<10;a++){
 // a = 10
 
 function start(){
-  console.log(a)
+  console.log(a) // afiseaza 10 de 10 ori
+}
+
+//v2 
+function runExemple(){
+  for(let index=0; index<10;index++){
+    // index=0, index=1,...index=9,
+    setTimeout(start, 100)
+  
+    function start(){
+      console.log(index) // se afiseaza de la 0..9 pentru ca index este in bloc scop
+    }
+  }
+}
+
+// v3
+function start(number){
+  console.log(number)
+}
+function runExemple(){
+  for(let index=0; index<10;index++){
+    // index=0, index=1,...index=9,
+    setTimeout(function(){
+      start(index)
+    }, 100)
+  }
 }
