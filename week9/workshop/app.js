1342807b218339e18fd0c44b64a790dbca4238fc
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-14 21:06:53 +0300

    Add getInput

---
+++ week9/workshop/app.js
@@ -0,0 +1,27 @@
+function HangMan (){
+  this.cuvinte = ["masina", "telecomanda", "telefon", "teleenciclopedie"];
+  var randomIndex = Math.floor(Math.random()*this.cuvinte.length);
+  this.cuvantAles = this.cuvinte[randomIndex];
+  console.log(this.cuvantAles);
+}
+
+/*
+Sa obtina inputul de la user
+1. Avem nevoie de un tag <input> 
+2. Referinta de la <input> 
+3. Un event de keydown, sa ascultam la acest event
+4. Afisam valoarea doar daca sa apasat enter
+*/
+HangMan.prototype.getInput = function () {
+  const userInput = document.querySelector('#userInput');
+  
+  userInput.addEventListener("keydown", function(event){
+    if(event.key == "Enter"){
+      console.log(event.target.value);
+    }
+  })
+}
+
+const hangMan = new HangMan();
+hangMan.getInput();
+
--- week9/workshop/readme.md
+++ week9/workshop/readme.md
@@ -1,5 +1,5 @@
 
-1. O lista de cuvinte din care extragem un cuvant random 
+1. [done] O lista de cuvinte din care extragem un cuvant random 
 2. Un mod de a introduce litere
 3. Sa verificam daca literea introdusa face parte din cuvant, daca da, afisam
 4. Daca gasim o literea, le afisam pe toate de acelasi tip
