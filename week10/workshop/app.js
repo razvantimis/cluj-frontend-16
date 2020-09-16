a4e05e4eacd1acfa90af3fa9e80ecdd6d693ee43
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-16 20:48:50 +0300

    Add renderUnderLines

--- week10/workshop/app.js
+++ week10/workshop/app.js
@@ -1,28 +1,65 @@
-function HangMan (){
-  this.cuvinte = ["masina", "telecomanda", "telefon", "teleenciclopedie"];
-  var randomIndex = Math.floor(Math.random()*this.cuvinte.length);
-  this.cuvantAles = this.cuvinte[randomIndex];
-  console.log(this.cuvantAles);
+function HangMan() {
+    this.cuvinte = ["masina", "telecomanda", "telefon", "teleenciclopedie"];
+    var randomIndex = Math.floor(Math.random() * this.cuvinte.length);
+    this.cuvantAles = this.cuvinte[randomIndex];
+    console.log('cuvantAles = ', this.cuvantAles);
+
+    // aici trebe sa apelam metoda care afiseaza in html liniutele
+}
+/*
+Sa afisam cate o liniuta pentru fiecare litera din cuvantul ales
+1. Parcurgem  fiecare literea din cuvantul ales
+2. Sa facem un div, care este defapt liniuta
+*/
+HangMan.prototype.renderUnderLines = function() {
+        const underLinesContainer = document.querySelector('#underLines');
+
+        underLinesContainer.style.display = 'flex'
+        underLinesContainer.style.flexDirection = 'row'
+        for (let index = 0; index < this.cuvantAles.length; index++) {
+            let underLineDiv = document.createElement('div');
+            underLineDiv.style.margin = '5px'
+                // innerText/innerHtml putem seta in string 
+            underLineDiv.innerText = '_';
+            // adaugam liniuta in container
+            underLinesContainer.appendChild(underLineDiv)
+        }
+    }
+    /*
+    Sa obtina inputul de la user
+    1. Avem nevoie de un tag <input> 
+    2. Referinta de la <input> 
+    3. Un event de keydown, sa ascultam la acest event
+    4. Afisam valoarea doar daca sa apasat enter
+    */
+HangMan.prototype.getInput = function() {
+    const userInput = document.querySelector('#userInput');
+
+    const self = this;
+    userInput.addEventListener("keydown", function(event) {
+        if (event.key == "Enter") {
+            const letter = event.target.value;
+            console.log('letter =', letter)
+            console.log('checkLetter =', self.checkLetter(letter))
+        }
+    })
 }
 
 /*
-Sa obtina inputul de la user
-1. Avem nevoie de un tag <input> 
-2. Referinta de la <input> 
-3. Un event de keydown, sa ascultam la acest event
-4. Afisam valoarea doar daca sa apasat enter
+Returneaza true daca litera apare in cuvantul ales, iar daca nu false
+Facem verificare in momentul in care utilizatorul apasa enter
 */
-HangMan.prototype.getInput = function () {
-  const userInput = document.querySelector('#userInput');
-  
-  userInput.addEventListener("keydown", function(event){
-    if(event.key == "Enter"){
-      const letter = event.target.value;
-      console.log(letter)
+HangMan.prototype.checkLetter = function(letter) {
+    for (let index = 0; index < this.cuvantAles.length; index++) {
+        const char = this.cuvantAles[index];
+        if (letter === char) {
+            return true
+        }
     }
-  })
+    return false
 }
+
 
 const hangMan = new HangMan();
 hangMan.getInput();
-
+hangMan.renderUnderLines();
--- week10/workshop/readme.md
+++ week10/workshop/readme.md
@@ -1,7 +1,8 @@
 
 1. [done] O lista de cuvinte din care extragem un cuvant random 
-2. Un mod de a introduce litere
-3. Sa verificam daca literea introdusa face parte din cuvant, 
-4. Afisam literea in html
-5. Daca gasim o literea, le afisam pe toate de acelasi tip
-6. Un number de vieti, daca ajunge la zero este game over
+2. [done] Un mod de a introduce litere
+3. [done] Sa verificam daca literea introdusa face parte din cuvant
+4. Sa afisam cate o liniuta pentru fiecare litera din cuvantul ales
+5. Daca literea apare in cuvant atunci punem pe liniuta cu litera
+6. Daca gasim o literea, le afisam pe toate de acelasi tip
+7. Un number de vieti, daca ajunge la zero este game over
