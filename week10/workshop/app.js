d4638bb83851a1b605dd7fd9ccb15ec78a19f2d0
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-21 19:52:48 +0300

    Add

--- week10/workshop/app.js
+++ week10/workshop/app.js
@@ -1,78 +1,80 @@
 function HangMan() {
-    this.cuvinte = ["masina", "telecomanda", "telefon", "teleenciclopedie"];
-    var randomIndex = Math.floor(Math.random() * this.cuvinte.length);
-    this.cuvantAles = this.cuvinte[randomIndex];
-    console.log('cuvantAles = ', this.cuvantAles);
-    this.underLines = {};
-    /*
-    {
-      'a': [ underLine1, underLine2],
-      'b': [ underLine1 ]
-    }
-    */
+  this.cuvinte = ["masina", "telecomanda", "telefon", "teleenciclopedie"];
+
+  // alegem cuvantul random
+  var randomIndex = Math.floor(Math.random() * this.cuvinte.length);
+  this.cuvantAles = this.cuvinte[randomIndex];
+  console.log('cuvantAles = ', this.cuvantAles);
+  this.underLines = {};
+  /*
+  {
+    'a': [ underLine1, underLine2],
+    'b': [ underLine1 ]
+  }
+  */
 }
 /*
 Sa afisam cate o liniuta pentru fiecare litera din cuvantul ales
 1. Parcurgem  fiecare literea din cuvantul ales
 2. Sa facem un div, care este defapt liniuta
 */
-HangMan.prototype.renderUnderLines = function() {
-        const underLinesContainer = document.querySelector('#underLines');
+HangMan.prototype.renderUnderLines = function () {
+  const underLinesContainer = document.querySelector('#underLines');
 
-        underLinesContainer.style.display = 'flex'
-        underLinesContainer.style.flexDirection = 'row'
-        for (let index = 0; index < this.cuvantAles.length; index++) {
-            let underLineDiv = document.createElement('div');
-            underLineDiv.style.margin = '5px'
-                // innerText/innerHtml putem seta in string 
-            underLineDiv.innerText = '_';
-            // adaugam liniuta in container
-            underLinesContainer.appendChild(underLineDiv);
+  underLinesContainer.style.display = 'flex'
+  underLinesContainer.style.flexDirection = 'row'
+  for (let index = 0; index < this.cuvantAles.length; index++) {
+    let underLineDiv = document.createElement('div');
+    underLineDiv.style.margin = '5px'
+    // innerText/innerHtml putem seta in string 
+    underLineDiv.innerText = '_';
+    // adaugam liniuta in container
+    underLinesContainer.appendChild(underLineDiv);
 
-            // salvam underLine
-            // char este pe rand fiecare literea din cuvantul ales
-            const char = this.cuvantAles[index];
+    // salvam underLine
+    // char este pe rand fiecare literea din cuvantul ales
+    const char = this.cuvantAles[index];
 
-            // initialiam valoare
-            if (!this.underLines[char]) {
-                this.underLines[char] = [];
-            }
-            this.underLines[char].push(underLineDiv);
-        }
-        console.log(this.underLines)
+    // initialiam valoare
+    if (!this.underLines[char]) {
+      this.underLines[char] = [];
     }
-    /*
-    Sa obtina inputul de la user
-    1. Avem nevoie de un tag <input> 
-    2. Referinta de la <input> 
-    3. Un event de keydown, sa ascultam la acest event
-    4. Afisam valoarea doar daca sa apasat enter
-    */
-HangMan.prototype.getInput = function() {
-    const userInput = document.querySelector('#userInput');
+    this.underLines[char].push(underLineDiv);
+  }
+  console.log(this.underLines)
+}
+/*
+Sa obtina inputul de la user
+1. Avem nevoie de un tag <input> 
+2. Referinta de la <input> 
+3. Un event de keydown, sa ascultam la acest event
+4. Afisam valoarea doar daca sa apasat enter
+*/
+HangMan.prototype.getInput = function () {
+  const userInput = document.querySelector('#userInput');
 
-    const self = this;
-    userInput.addEventListener("keydown", function(event) {
-        if (event.key == "Enter") {
-            const letter = event.target.value;
-            console.log('letter =', letter)
-            self.replaceUnderLine(letter)
-        }
-    })
+  const self = this;
+  userInput.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+      const letter = event.target.value;
+      console.log('letter =', letter)
+      self.replaceUnderLine(letter)
+    }
+  })
 }
 
 /*
 Returneaza true daca litera apare in cuvantul ales, iar daca nu false
 Facem verificare in momentul in care utilizatorul apasa enter
 */
-HangMan.prototype.checkLetter = function(letter) {
-    for (let index = 0; index < this.cuvantAles.length; index++) {
-        const char = this.cuvantAles[index];
-        if (letter === char) {
-            return true
-        }
+HangMan.prototype.checkLetter = function (letter) {
+  for (let index = 0; index < this.cuvantAles.length; index++) {
+    const char = this.cuvantAles[index];
+    if (letter === char) {
+      return true
     }
-    return false
+  }
+  return false
 }
 
 /*
@@ -100,15 +102,13 @@ Input:
 
 // }
 
-HangMan.prototype.replaceUnderLine = function(letter) {
-    if (this.checkLetter(letter)) {
-        // vrem sa obtinem underLine din DOM fara sa facem for
-        const underLines = this.underLines[letter];
-        underLines.forEach(function(underLineDiv) {
-            console.log(underLineDiv)
-            underLineDiv.innerText = letter;
-        })
-    }
+HangMan.prototype.replaceUnderLine = function (letter) {
+  if (this.checkLetter(letter)) {
+    // vrem sa obtinem underLine din DOM fara sa facem for
+    this.underLines[letter].forEach(function (div) {
+      div.innerText = letter;
+    })
+  }
 
 }
 
--- week10/workshop/readme.md
+++ week10/workshop/readme.md
@@ -3,6 +3,6 @@
 2. [done] Un mod de a introduce litere
 3. [done] Sa verificam daca literea introdusa face parte din cuvant
 4. [done] Sa afisam cate o liniuta pentru fiecare litera din cuvantul ales
-5. Daca literea apare in cuvant atunci punem pe liniuta cu litera
-6. Daca gasim o literea, le afisam pe toate de acelasi tip
+5. [done] Daca literea apare in cuvant atunci punem in locul liniutei cu litera
+6. [done] Daca gasim o literea, le afisam pe toate de acelasi tip
 7. Un number de vieti, daca ajunge la zero este game over
