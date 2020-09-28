fde856114c880b7eff94ba60848c26cb42b2c991
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-28 18:51:34 +0300

    Add

---
+++ week12/es6 - part 2/static-method.js
@@ -0,0 +1,62 @@
+// inaite de es6
+function Car(type){
+ this.type = type
+}
+
+Car.prototype.start = function(){
+  console.log('start')
+  console.log('start() => speed limit =', Car.SPEED_LIMIT)
+}
+Car.SPEED_LIMIT = 200; // var SPEED_LIMIT = 200
+Car.clean = function(){
+  console.log('clean')
+}
+
+const car1 = new Car();
+console.log('global => speed limit =', Car.SPEED_LIMIT)
+
+
+// es6
+
+class Car {
+  static SPEED_LIMIT = 200;
+
+  static clean(){
+    console.log('clean')
+  }
+
+  constructor(type){
+    this.type = type;
+    // this.SPEED_LIMIT = 200;
+  }
+
+  start(){
+    console.log('start')
+    console.log('start() => speed limit =', Car.SPEED_LIMIT)
+  }
+
+
+}
+
+console.log(Car.clean());
+
+const car1 = new Car();
+const car2 = new Car();
+
+
+// real exemple
+
+class ServerApi {
+  async static getPosts() {
+    const response = await fetch('http://localhost:3000/posts');
+    return response.json()
+  }
+
+  async static deletePost(postId) {
+    const response = await fetch(`http://localhost:3000/posts/${postId}`, { method: 'DELETE' })
+    return response.json();
+  }
+}
+
+
+ServerApi.getPosts()
