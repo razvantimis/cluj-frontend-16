1778502914f5e72ba821a58c6d7d758e9c080408
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-28 21:07:04 +0300

    Add

--- week12/player/app.js
+++ week12/player/app.js
@@ -3,6 +3,29 @@ class Player {
     this.gameContainer = gameContainer;
   }
 
+  /*
+   Vream sa apara un div care va fi player in gameContainer
+   Pentru inceput, il punem intr-un colt din container
+   Div va avea pozitie absoluta si va ajucati cu x si y
+  */
+  createHtml(){
+    
+  }
+
+  // eventul il inregistram pe window/document
+  // verificare de keydown - folositi switch/ if else
+  registerMoving(){
+  }
+
+  movePlayerHtml(){
+    
+  }
+
+  // returneaza true sau false
+  checkIfPlayerIsOutsideOfContainer(){
+
+  }
+
 }
 
 const gameContainer = document.querySelector('#gameContainer');
