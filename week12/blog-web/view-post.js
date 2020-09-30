897aaad339c8659683db83c1b71238a755f16e40
Razvan Timis <timisrazvanvasile@gmail.com>
2020-09-30 19:53:48 +0300

    Add post view

--- week12/blog-web/view-post.js
+++ week12/blog-web/view-post.js
@@ -1,7 +1,13 @@
 // 1. Obtinem id din url
 const idPost = window.location.search.substring(4);
-console.log(idPost);
+console.log("idPost = ", idPost);
 // 2. Facem requestul la server dupa id
-ServerApi.getPostById(idPost).then(function (response) {
-  console.log(response)
+ServerApi.getPostById(idPost).then(function (post) {
+  console.log("post =", post)
+  // 3. Sa afisam in html continu unui post
+  const postContainer = document.getElementById("postContainer");
+  postContainer.innerHTML = `
+  <h3>${post.title}</h3>
+  <p>${post.text} </p>
+  `
 })
