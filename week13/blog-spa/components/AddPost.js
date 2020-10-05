2cad28c6911d9e15ab2577539bfc74048e6d0f45
Razvan Timis <timisrazvanvasile@gmail.com>
2020-10-05 20:11:09 +0300

    Add post

--- week13/blog-spa/components/AddPost.js
+++ week13/blog-spa/components/AddPost.js
@@ -3,7 +3,33 @@ export default class AddPost {
   render(container) {
     const addPostContainer = document.createElement('div');
 
-    addPostContainer.innerText = " Aici va fi adaugarea de posturi"
+    addPostContainer.innerHTML = ` 
+  <div class="input-section">
+    Titlu: <input type="text" id="postTitle">
+  </div>
+  <div class="input-section">
+    Text: <textarea type="text" id="postText" rows="10"></textarea>
+  </div>
+  <div class="input-section">
+    Autor: <input type="text" id="postAuthor">
+  </div>
+  <div class="input-section">
+    Date: <input type="text" id="postDate">
+  </div>
+`
+    const addButton = document.createElement('button');
+    addButton.innerText = "Adauga post"
+
+    addPostContainer.appendChild(addButton);
+
+    addButton.addEventListener('click', function () {
+      console.log('sa dat click')
+    })
+
+    const postTitle = addPostContainer.querySelector('#postTitle');
+    console.log(postTitle);
+
+
 
     container.appendChild(addPostContainer);
   }
