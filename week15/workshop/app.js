8950703a5fbaa27d55ea51ca99c60f349565ceec
Razvan Timis <timisrazvanvasile@gmail.com>
2020-10-21 18:13:54 +0300

    Init empty game react

---
+++ week15/workshop/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Gol
+      </div>
+    )
+  }
+}
+
+const appDOM = document.getElementById('app');
+ReactDOM.render(<App />, appDOM)
